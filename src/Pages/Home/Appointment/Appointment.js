86567bbaf77dc16cc5c8248e73c187b8744dd487
Rony <frrony1996@gmail.com>
2022-05-10 23:45:31 +0500

    appointment-mad

--- src/Pages/Home/Appointment/Appointment.js
+++ src/Pages/Home/Appointment/Appointment.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import appointmentpic from "../../../assets/images/appp-doctor (1).png"
+import appointmentpic from "../../../assets/images/doctor-small.png"
+import bgPic from "../../../assets/images/appointment.png"
+
 const Appointment = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={appointmentpic} alt="doctor-pic" />
-                <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+        <div class="hero mt-40">
+            <img style={{ height: "396px" }} className="w-full" src={bgPic} alt="background-pic" />
+            <div class="hero-content flex-col lg:flex-row">
+                <img className='relative bottom-32 hidden lg:block' src={appointmentpic} alt="doctor-pic" />
+                <div className='text-white'>
+                    <p className='text-primary mb-2'>Appointment</p>
+                    <h1 class="text-5xl font-bold">Make Your Appointment Now</h1>
+                    <p class="py-6">Our service is free for all the time. You can book your appointment any time. Our assistants are very cooperative.</p>
+                    <button className="btn font-bold text-white border-0 bg-gradient-to-r from-primary to-secondary">Book Now</button>
                 </div>
             </div>
         </div>
